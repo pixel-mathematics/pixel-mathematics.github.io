bdf0455aa138b7738c7c7027bf888d311fcc444c
dm1nh <dangminhngo.dev@gmail.com>
2026-09-03 22:20:57 +0700

    initial commit

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,11 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { format } from "date-fns"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function formatDate(date: Date, fmt: string = "dd/MM/yyyy") {
+  return format(date, fmt)
 }
