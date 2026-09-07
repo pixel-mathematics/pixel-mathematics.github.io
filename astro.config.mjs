281257296f4c82ebfa5f01a97b1fc9857d1962cc
dm1nh <dangminhngo.dev@gmail.com>
2026-09-07 12:26:02 +0700

    auto deployment with action

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,11 @@ import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from "astro/config"
 import react from "@astrojs/react"
 
-import mdx from "@astrojs/mdx";
+import mdx from "@astrojs/mdx"
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://pixel-mathematics.github.io",
   vite: {
     plugins: [tailwindcss()],
   },
