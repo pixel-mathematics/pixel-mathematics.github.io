027328ba905d61d0a4ebc25e937736c93e1e770b
dm1nh <dangminhngo.dev@gmail.com>
2026-09-12 20:02:53 +0700

    add tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import babel from "@rolldown/plugin-babel";
+import tailwindcss from "@tailwindcss/vite";
 import tanstackRouter from "@tanstack/router-plugin/vite";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
@@ -12,6 +13,7 @@ export default defineConfig({
       autoCodeSplitting: true,
     }),
     react(),
+    tailwindcss(),
     babel({ presets: [reactCompilerPreset()] }),
     tsconfigPaths(),
   ],
